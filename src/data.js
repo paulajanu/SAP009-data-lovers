2c1ac13f9d0d5654fce847f47c3e00683d84e066
Ana <ana_leticia@geb.inatel.br>
2023-02-10 10:14:46 -0300

    finalização das histórias e filtro pesquisar por nome.
    Co-authored-by: paulajanu <annapsjanuario@gmail.com>

--- src/data.js
+++ src/data.js
@@ -1,9 +1,21 @@
 // estas funciones son de ejemplo
+//export const example = () => {
+  //return 'example';
+//};
+//export const anotherExample = () => {
+ // return 'OMG';
+//};
+//   const  resultadoLocalização  =  data.filter( ( personagem )  =>  personagem . nome . toLowerCase ( ) . includes ( condição . toLowerCase ( ) ) ) ;
+//   return  resultadoLocalização ;
+// } ;
 
-export const example = () => {
-  return 'example';
-};
+// export const nameFilter = (data, search) => data.filter(character => {
+//   return character.name.toLowerCase().includes(search.toLowerCase());
+// })
 
-export const anotherExample = () => {
-  return 'OMG';
+//export const nameFilter = (data, search) => data.filter(item => item.title.toUpperCase().includes(search.toUpperCase()))
+
+export const filtroNomes = (dados, nomePesquisado) => {
+  return dados.filter(campeao => campeao.name.toUpperCase().includes(nomePesquisado)); //poderia ser id ou name
+  
 };
--- src/main.js
+++ src/main.js
@@ -1,14 +1,16 @@
+import {filtroNomes} from './data.js';
 import data from './data/lol/lol.js';
 
 //const lol = Object.keys(data.data); //tranformando objeto em um array
 //console.log(teste.); //deu certo: array de strings
 
-let lol = [];
+let dadosLol = [];
 for (let character in data.data){
-  lol.push(data.data[character]);
+  dadosLol.push(data.data[character]);
 }
+
 const root = document.getElementById("info-cards");
-//testeeeee
+
 function infosDosCardsTela (data) {
   root.innerHTML = data.map((character) => `
     <div class="cards">
@@ -37,9 +39,32 @@ function infosDosCardsTela (data) {
       </div>
     </div>
   `).join('');
-  
 }
+infosDosCardsTela(dadosLol);
 
-infosDosCardsTela(lol);
+
+//PESQUISAR ESTÁ FUNCIONADO!
+const campoPesquisar = document.getElementById("txt-pesquisa");
+campoPesquisar.addEventListener("input", event => { //input é melhor que keypress
+  const nomeDoCampeao = event.target.value.toUpperCase();  //event.target.value trabalham juntos. Para gravar o conteudo digitado usa esse event, o target é a referencia ao objeto que enviou o evento, e o .value é o valor (objeto+conteudo+valor)
+  const filtrarCampeoes = filtroNomes(dadosLol, nomeDoCampeao); //chamada da função no arquivo data.js
+  infosDosCardsTela(filtrarCampeoes);
+});
 
 
+const ordenarPor = document.getElementById("ordenar");
+ordenarPor.addEventListener("click", () => {
+    console.log("ordenar: ok");
+});
+
+
+const buscarPorFuncao = document.getElementById("buscar-funcao");
+buscarPorFuncao.addEventListener("click", () => {
+    console.log("buscar por função: ok");
+});
+
+
+const buscarPorDificuldade = document.getElementById("buscar-dificuldade");
+buscarPorDificuldade.addEventListener("click", () => {
+    console.log("buscar por dificuldade: ok");
+});
